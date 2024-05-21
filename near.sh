742ef6c1d5ff390add4a11af3111f91198f5e01f
Yudistira <90891788+Ypx2@users.noreply.github.com>
2024-05-21 12:06:56 +0700

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-668fd760-1dc6-4491-9639-3cf5428d1094/"
 
 OUTPUT_DIR="logs"
 
